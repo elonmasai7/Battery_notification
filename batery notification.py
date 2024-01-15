6186a60dfda31fe011cbec0fece8196c21108d97
elonmasai <elonmasai7@gmail.com>
2024-01-15 20:12:26 +0300

    notification

---
+++ batery notification.py
@@ -0,0 +1,14 @@
+import psutil
+
+batery = psutil.sensors_battery()
+plugged = batery.power_unplugged
+percent = batery.percent
+
+if percent ≤ 30 and plugged≠true:
+  from pynotifier import Notification
+  Notification(
+    title = "Battery low"
+    description=str(percent) + "% Battery remainin!!",
+    duration=5, 
+  ).send()
+  
